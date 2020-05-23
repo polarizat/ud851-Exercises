78b0f97ce40026affae8d824d9fecdf821757242
Sergiu Apostul <sergiu.apostul@gmail.com>
2020-05-23 21:07:54 +0300

    Migrate to AndroidX and update dependencies. Solved all TODOs for T04b.02

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,7 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -49,11 +49,17 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String address = "Oituz 1 Timisoara, RO";
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme("geo")
+                .path("0.0")
+                .query(address);
+        Uri addressUri = uriBuilder.build();
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(addressUri);
     }
 
     /**
@@ -114,6 +120,12 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called showMap with a Uri as the single parameter
     // Do steps 2 - 4 within the showMap method
+    void showMap (Uri uri){
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
         // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
 
         // TODO (3) Set the data of the Intent to the Uri passed into this method
