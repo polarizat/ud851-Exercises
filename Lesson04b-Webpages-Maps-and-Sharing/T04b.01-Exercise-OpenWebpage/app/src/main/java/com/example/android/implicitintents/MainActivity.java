1426833681403eee30fb248f074627fa6cbfad5d
Sergiu Apostul <sergiu.apostul@gmail.com>
2020-05-23 20:48:08 +0300

    Migrate to AndroidX and update dependencies. Solved all TODOs for T04b.01

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -36,9 +38,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenWebpageButton(View v) {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
+        String urlAsString = "http://www.udacity.com";
 
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        openWebPage(urlAsString);
     }
 
     /**
@@ -79,6 +82,14 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called openWebPage that accepts a String as a parameter
     // Do steps 2 - 4 within openWebPage
+    private void openWebPage(String url){
+        Uri webPage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
 
         // TODO (2) Use Uri.parse to parse the String into a Uri
 
